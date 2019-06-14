260fb56493023a958c7ae484682330b57fc2795b
Hay00 <vine.vinedutra@hotmail.com>
2019-06-14 19:32:14 -0300

    Up Base

---
+++ codeIgniter/application/controllers/Armas.php
@@ -0,0 +1,39 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Armas extends CI_Controller
+{
+    public function __construct()
+    {
+        //Chama modelo para carregar dados
+        parent::__construct();
+        //$this->load->model('');
+    }
+    public function index()
+    {
+        //Chama a view pra carregar html
+        // $this->load->view('');
+    }
+
+    public function adicionar()
+    {
+        if($_POST){
+            $this->Armas_model->inserir($_POST);
+        }
+
+        $this->load->view('bootstrap');
+        $this->load->view('Armas_adicionar');
+    }
+
+    public function atualizar($id)
+    {
+
+        $this->load->view('bootstrap');
+        $this->load->view('Armas_adicionar');
+    }
+
+    public function remover($id)
+    { 
+
+    }
+}
