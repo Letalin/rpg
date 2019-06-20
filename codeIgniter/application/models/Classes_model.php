f0776ee9719c4e852b0e2d3a5fda2e7a59995547
Hay00 <vine.vinedutra@hotmail.com>
2019-06-20 16:49:38 -0300

    Inimigos por tabela

--- codeIgniter/application/models/Classes_model.php
+++ codeIgniter/application/models/Classes_model.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Classe_model extends CI_Model
+class Classes_model extends CI_Model
 {
     public function __construct()
     {
